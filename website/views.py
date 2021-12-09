6e086e45b3cf31f35f6713120409f6586d5f6552
Jordan <jmmhqw@mst.edu>
2021-12-09 07:42:14 -0600

    tables working (added some table files, might not use them)

--- website/views.py
+++ website/views.py
@@ -11,7 +11,7 @@ views = Blueprint('views', __name__)
 def home():
     if current_user.isArtist():
         with engine.connect() as conn:
-            songTable = conn.execute(
+            songTableP = conn.execute(
                 text("SELECT Songs.songName, Albums.albumName, Songs.genre, AVG(RATES.rating) "
                      "FROM Songs, Albums, RATES, HAS, IS_ON "
                      "WHERE Songs.songID = IS_ON.songID "
@@ -22,7 +22,7 @@ def home():
                      "GROUP BY Songs.songName"),
                 {"artistID": current_user.get_id()}
             )
-            albumTable = conn.execute(
+            albumTableP = conn.execute(
                 text("SELECT Albums.albumName, Albums.numTracks, Albums.genre, AVG(RATES.rating) "
                      "FROM Albums, RATES, HAS, IS_ON "
                      "WHERE Albums.albumID = IS_ON.albumID "
@@ -32,16 +32,20 @@ def home():
                 {"artistID": current_user.get_id()}
             )
             print(current_user.User_ID)
-            for row in songTable:
+            songTable = []
+            albumTable = []
+            print("songTable:")
+            for row in songTableP:
+                songTable.append(row)
                 print(row)
-            for row in albumTable:
+            print("albumTable")
+            for row in albumTableP:
+                albumTable.append(row)
                 print(row)
 
     else:
-        songTable = []
-        print(current_user.User_ID)
         with engine.connect() as conn:
-            librarySongTable = conn.execute(
+            songTableP = conn.execute(
                 text("SELECT Songs.songName, Albums.albumName, Songs.genre, RATES.rating, Users.username "
                      "FROM Songs, Albums, RATES, Users, Artists, HAS, CREATES, CONTAINS, IS_ON "
                      "WHERE Songs.songID = CONTAINS.songID "
@@ -56,13 +60,29 @@ def home():
                      "GROUP BY Songs.songName"),
                 {"listenerID": current_user.get_id()}
             )
-            for row in librarySongTable:
+            albumTableP = conn.execute(
+                text("SELECT Albums.albumName, Albums.numTracks, Albums.genre, RATES.rating, Users.username "
+                     "FROM Albums, RATES, Users, Artists, HAS, CREATES, CONTAINS, IS_ON "
+                     "WHERE Albums.albumID = CONTAINS.albumID "
+                     "AND CONTAINS.libID = CREATES.libID "
+                     "AND CREATES.listenerID = :listenerID "
+                     "AND Albums.albumID = RATES.albumID "
+                     "AND Albums.albumID = IS_ON.albumID "
+                     "AND IS_ON.artistID = Artists.User_ID "
+                     "AND Artists.User_ID = Users.User_ID "
+                     "GROUP BY Albums.albumName"),
+                {"listenerID": current_user.get_id()}
+            )
+            print(current_user.User_ID)
+            songTable = []
+            albumTable = []
+            print("songTable:")
+            for row in songTableP:
+                songTable.append(row)
                 print(row)
-            #libraryAlbumTable = conn.execute(
-            #    text(),
-            #    {"listenerID": current_user.get_id()}
-            #)
+            print("albumTable:")
+            for row in albumTableP:
+                albumTable.append(row)
+                print(row)
 
-
-
-    return render_template("home.html", user=current_user, table=songTable)
+    return render_template("home.html", user=current_user, songTable=songTable, albumTable=albumTable)
